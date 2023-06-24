3d482a19f1dac47c261c9e0bd87b906ee4a4ebdf
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-06-24 18:39:53 -0300

    Criado componente de CurrentWeather estático

---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,65 @@
+import React from "react"
+import {View, Text, StyleSheet} from 'react-native'
+import { Feather } from '@expo/vector-icons';
+
+const CurrentWeather = () => {
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.container}>
+        <Feather name="sun" size={100} color="black" />
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style={styles.message}>It's perfect t-shirt weather</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'red',
+    paddingTop: 32
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  temp: {
+    fontSize: 48,
+    color: 'black'
+  },
+  feels: {
+    fontSize: 30,
+    color: 'black'
+  },
+  highLow: {
+    fontSize: 20,
+    color: 'black'
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    paddingLeft: 24,
+    marginBottom: 32
+  },
+  description: {
+    fontSize: 48,
+  },
+  message: {
+    fontSize: 30,
+  }
+})
+
+export default CurrentWeather
